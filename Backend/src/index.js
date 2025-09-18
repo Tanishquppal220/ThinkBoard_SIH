9e54689054f444770721e90b29e55561ef858b28
Abhishek Kumar Pundir <abhishekgigaiw@gmail.com>
2025-09-18 17:17:01 +0530

    working code

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -32,7 +32,7 @@ app.use('/api/user', userRoutes);
 app.use('/api/message',messageRoutes);
 if (process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, "../Frontend/dist")));
-    app.get("/*", (req, res) => {
+    app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../Frontend/dist/index.html"));
 });
 
