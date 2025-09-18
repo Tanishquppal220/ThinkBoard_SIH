17ad273e4871bd0a7d5e0bdd20a08431c7d1085f
Abhishek Kumar Pundir <abhishekgigaiw@gmail.com>
2025-09-18 15:33:59 +0530

    possible error handled

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -30,7 +30,7 @@ app.use('/api/notes',notesRoutes);
 app.use('/api/auth',authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/message',messageRoutes);
-if (process.env.Node_ENV === "production"){
+if (process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
     app.get("*",(req,res)=>{
         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
